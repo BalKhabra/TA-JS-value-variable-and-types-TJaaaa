ec0264ce3f50b95e13d044ea756934e2a07699ce
Baljit Khabra <baljitkhabra99@gmail.com>
2022-02-27 21:56:54 -0800

    did loop assignment

--- block-BJaaay/code/index.js
+++ block-BJaaay/code/index.js
@@ -3,6 +3,8 @@
 // Do the following using loops:
 
 // Using `console.log` log all the values from 1 to 10.
+for (let i = 1; i < 10; i = i+1)
+console.log (i)
 
 // Using `console.log` log all the evan values from 1 to 10.
 
